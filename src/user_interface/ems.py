94b858305bed095aff2860e391afa74a4b089012
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2024-06-09 19:49:45 +0200

    added left frame, id and name labels

--- src/user_interface/ems.py
+++ src/user_interface/ems.py
@@ -27,5 +27,18 @@ heading_label = customtkinter.CTkLabel(
 )
 heading_label.grid(row=0, column=1)
 
+left_frame = customtkinter.CTkFrame(window)
+left_frame.grid(row=1, column=0)
+
+id_label = customtkinter.CTkLabel(left_frame, text="Id", font=("arial", 15, "bold"))
+id_label.grid(row=0, column=0, padx=20, sticky="w")
+id_value = customtkinter.CTkEntry(left_frame, font=("arial", 15, "bold"), width=180)
+id_value.grid(row=0, column=1, pady=20)
+
+name_label = customtkinter.CTkLabel(left_frame, text="Name", font=("arial", 15, "bold"))
+name_label.grid(row=1, column=0, padx=20, sticky="w")
+name_value = customtkinter.CTkEntry(left_frame, font=("arial", 15, "bold"), width=180)
+name_value.grid(row=1, column=1, pady=20)
+
 
 window.mainloop()
