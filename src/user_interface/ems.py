b30fb306d65253903da3b741a1eaa9a1f8f73f9c
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2024-06-09 21:20:23 +0200

    removed appearance and theme setup

--- src/user_interface/ems.py
+++ src/user_interface/ems.py
@@ -4,9 +4,6 @@ import customtkinter
 from PIL import Image
 
 
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("blue")
-
 window = customtkinter.CTk()
 window.geometry("1230x678")
 window.resizable(False, False)
