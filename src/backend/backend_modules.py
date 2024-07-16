db7187be42b3492a01854ebb7efffceab3b496c0
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2024-07-17 00:46:59 +0200

    small fix

--- src/backend/backend_modules.py
+++ src/backend/backend_modules.py
@@ -1,13 +1,18 @@
 """"Backend module"""
+from typing import Any, Dict
+
 from src.backend import httpx_client
 
 
-def verify_user(username: str) -> None:
+headers = {"accept": "application/json"}
+
+
+def verify_user(username: str) -> Dict[str, Any]:
     """Verify user before login."""
-    headers = {"accept": "application/json"}
-    response = httpx_client.httpx_client.get(
-        f"http://localhost:8000/verify_user/v1/?username={username}",
-        headers=headers,
-    )
+    url = "http://localhost:8000/verify_user/v1/"
+    params = {
+        "username": username,
+    }
+    response = httpx_client.httpx_client.get(url, params=params, headers=headers)
     response.raise_for_status()
     return response.json()
