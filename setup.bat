cb2ff6fb61dde4e34cb4960b73a8e870cfab5942
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2025-08-27 02:01:16 +0200

    setup bash script

--- setup.bat
+++ setup.bat
@@ -1,33 +1,3 @@
-@REM @ECHO OFF
-
-@REM REM Force execution from the batch file's folder (project root)
-@REM cd /d "%~dp0"
-
-
-@REM @REM Check if image 'docker/ems' exists
-@REM SET IMAGE_NAME=docker/ems
-@REM FOR /F "tokens=*" %%i IN ('docker images -q %IMAGE_NAME%') DO SET IMAGE_EXISTS=%%i
-
-@REM IF NOT DEFINED IMAGE_EXISTS (
-@REM     echo Image %IMAGE_NAME% found, building %IMAGE_NAME% ...
-@REM     docker build -t %IMAGE_NAME% .
-@REM     echo Image build complete.
-@REM ) ELSE (
-@REM     echo Image %IMAGE_NAME% already exists.
-@REM )
-
-@REM @REM Start Docker Containers
-@REM echo Starting Docker Containers...
-@REM docker-compose up -d
-
-@REM @REM Activate the virtual environment
-@REM CALL env\Scripts\activate
-
-@REM @REM Running Streamlit UI
-@REM ECHO Starting Streamlit UI ...
-@REM python -m streamlit run src/ems.py
-
-@REM ###########################################################################################################
 @ECHO OFF
 REM Force execution from the batch file's folder (project root)
 cd /d "%~dp0"
