b763d93ebe8855334d82984e8031c621de0a431d
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2026-02-22 02:34:02 +0100

    updated endpoints to prevent sql injection

--- api/employees.py
+++ api/employees.py
@@ -1,16 +1,24 @@
-"""Module to handle employee data"""
+"""
+This module defines the API endpoints for employee data management, including retrieval and addition of employee records.
+The endpoints allow clients to fetch employee data based on various criteria such as ID, first name, last name, department,
+and position. Additionally, it provides an endpoint to add new employee records to the database.
+"""
 from datetime import date
 from enum import Enum
 from typing import Any, Dict, List
 
 from fastapi import APIRouter, HTTPException
 from psycopg2 import sql
-from psycopg2.errors import OperationalError, UniqueViolation, InFailedSqlTransaction
+from psycopg2.errors import (
+    OperationalError,
+    UniqueViolation,
+    InFailedSqlTransaction,
+)
 from pydantic import BaseModel
 
-from src.backend import db_connect
-from src.config import settings
-from src.log_handler import logger
+from app.config.config import settings
+from app.logger.log_handler import logger
+from backend import db_connect
 
 
 router = APIRouter(prefix="/v1", tags=["Employee Data"])
@@ -26,36 +34,36 @@ class DepartmentIdRequest(str, Enum):
 
 
 class EmployeeCreateRequest(BaseModel):
-    first_name: str
-    middle_name: str
-    last_name: str
-    address: str
-    date_of_birth: str
-    gender_id: int
     phone: str
-    position_id: int
     email: str
-    department_id: int
-    salary: float
-    hired_date: str
     status: str
+    address: str
+    salary: float
+    gender_id: int
+    last_name: str
+    first_name: str
+    hired_date: str
+    position_id: int
+    middle_name: str
+    date_of_birth: str
+    department_id: int
 
 
 class EmployeeResponseModel(BaseModel):
     id: int
-    first_name: str
-    middle_name: str | None = None
-    last_name: str
     email: str
     phone: str
-    address: str
-    salary: float
-    department: str
-    position: str
     gender: str
-    date_of_birth: date
-    hired_date: date
     status: str
+    address: str
+    position: str
+    salary: float
+    last_name: str
+    department: str
+    first_name: str
+    hired_date: date
+    date_of_birth: date
+    middle_name: str | None = None
     date_resigned: date | None = None
 
 
@@ -77,17 +85,31 @@ class PositionIdRequest(str, Enum):
     senior_engr_mgr = "Senior Engineering Manager"
 
 
-def fetch_employee_data(where_clause: str, value: Any) -> List[Dict[str, Any]]:
+ALLOWED_EMPLOYEE_FILTERS = {
+    "id": "e.id",
+    "first_name": "e.first_name",
+    "last_name": "e.last_name",
+    "department": "d.department",
+    "status": "e.status"
+}
+
+
+def fetch_employee_data(filter_field: str, value: Any) -> List[Dict[str, Any]]:
     """
     A helper function for fetching employee data via API calss to the Database
 
-    :param where_clause: Conditional filter
+    :param filter_field: The field to filter on (e.g., "id", "first_name")
     :param value: value for filter
     :return: List of dictionary values for retrieved data.
     """
+
+    if filter_field not in ALLOWED_EMPLOYEE_FILTERS:
+        raise ValueError("Invalid filter field")
+
+    column = ALLOWED_EMPLOYEE_FILTERS[filter_field]
+
     with db_connect.db_client.cursor() as cursor:
-        query = sql.SQL(
-            f"""
+        query = sql.SQL("""
                 SELECT
                     e.id,
                     e.first_name,
@@ -104,12 +126,22 @@ def fetch_employee_data(where_clause: str, value: Any) -> List[Dict[str, Any]]:
                     e.hired_date,
                     e.status,
                     e.date_resigned
-                FROM {settings.employee_table_name} AS e
-                JOIN {settings.dept_table_name} AS d ON e.{settings.department_id} = d.{settings.fetch_employee_data_join_column}
-                JOIN {settings.gender_table_name} AS g ON e.{settings.gender_id} = g.{settings.fetch_employee_data_join_column}
-                JOIN {settings.position_table_name} AS p ON e.{settings.position_id} = p.{settings.fetch_employee_data_join_column}
-                WHERE {where_clause};
+                FROM {employee} AS e
+                JOIN {department} AS d ON e.{department_id} = d.{data_join_column}
+                JOIN {gender} AS g ON e.{gender_id} = g.{data_join_column}
+                JOIN {position} AS p ON e.{position_id} = p.{data_join_column}
+                WHERE {filter_column} = %s;
             """
+            ).format(
+                employee=sql.Identifier(settings.employee_table_name),
+                department=sql.Identifier(settings.dept_table_name),
+                department_id=sql.Identifier(settings.department_id),
+                gender=sql.Identifier(settings.gender_table_name),
+                gender_id=sql.Identifier(settings.gender_id),
+                position=sql.Identifier(settings.position_table_name),
+                position_id=sql.Identifier(settings.position_id),
+                data_join_column=sql.Identifier(settings.fetch_employee_data_join_column),
+                filter_column=sql.SQL(column)
             )
         cursor.execute(query, (value,))
         rows = cursor.fetchall()
@@ -129,7 +161,7 @@ async def get_employee_data_by_id(employee_id: int) -> List[Dict[str, Any]]:
     """
     logger.info(f"Retrieving employee data. ID: {employee_id}")
     result = fetch_employee_data(
-        where_clause="e.id = %s",
+        filter_field="id",
         value=employee_id
     )
 
@@ -150,7 +182,7 @@ async def get_employee_data_by_first_name(first_name: str) -> List[Dict[str, Any
     """
     logger.info(f"Retrieving employee data. first name: {first_name.capitalize()}")
     result = fetch_employee_data(
-        where_clause="e.first_name = %s",
+        filter_field="first_name",
         value=first_name.capitalize()
     )
 
@@ -171,7 +203,7 @@ async def get_employee_data_by_last_name(last_name: str) -> List[Dict[str, Any]]
     """
     logger.info(f"Retrieving employee data. last name: {last_name.capitalize()}")
     result = fetch_employee_data(
-        where_clause="e.last_name = %s",
+        filter_field="last_name",
         value=last_name.capitalize()
     )
 
@@ -192,7 +224,7 @@ async def get_employee_data_by_department(department: DepartmentIdRequest) -> Li
     """
     logger.info(f"Retrieving employee data. department: {department}")
     result = fetch_employee_data(
-        where_clause="d.department = %s",
+        filter_field="department",
         value=department
     )
 
@@ -211,9 +243,9 @@ async def get_employee_data_by_position(position: PositionIdRequest) -> List[Dic
     :param position: Employee position
     :return: Employee data from all tables, if available.
     """
-    logger.info(f"Retrieving employee data. department: {position}")
+    logger.info(f"Retrieving employee data. position: {position}")
     result = fetch_employee_data(
-        where_clause="p.position = %s",
+        filter_field="position",
         value=position
     )
 
